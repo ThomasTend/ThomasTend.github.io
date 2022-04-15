a79c934f2246507f387aa8a67b2b22c5152f9f29
ThomasTend <thomas.tendron@mail.mcgill.ca>
2022-04-16 00:00:29 +0100

    full version developped locally

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ export const routes: Routes = [
   {
       path: 'research',
       component: ResearchComponent
-  }
+  },
+  { path: '',   redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
