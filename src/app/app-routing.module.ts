1fccc577aec16d8570e21a431574a6fa7b0d16c0
ThomasTend <thomas.tendron@mail.mcgill.ca>
2024-11-16 22:12:41 +0000

    Removed most content.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ResearchComponent } from './research/research.component';
 
 export const routes: Routes = [
   {
       path: 'home',
       component: HomeComponent
-  },
-  {
-      path: 'research',
-      component: ResearchComponent
   },
   { path: '',   redirectTo: 'home', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ResearchComponent } from './research/research.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ResearchComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
